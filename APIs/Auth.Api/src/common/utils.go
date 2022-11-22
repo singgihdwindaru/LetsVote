02163e21ae64fac387e92b36b330138662edd69b
singgihdwindaru <singgihlab@gmail.com>
2022-11-22 17:16:18 +0700

    add func random int for nonce

--- APIs/Auth.Api/src/common/utils.go
+++ APIs/Auth.Api/src/common/utils.go
@@ -1,3 +1,42 @@
 package common
 
 // TBD
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+var RandomStringGenerator = func() (string, error) {
+	nonceBytes := make([]byte, 16)
+	_, err := rand.Read(nonceBytes)
+	if err != nil {
+		return "", fmt.Errorf("could not generate nonce")
+	}
+	result := base64.URLEncoding.EncodeToString(nonceBytes)
+	return result, nil
+}
+
+var RandomIntGenerator = func(numberOfDigits int) (int, error) {
+	maxLimit := int64(int(math.Pow10(numberOfDigits)) - 1)
+	lowLimit := int(math.Pow10(numberOfDigits - 1))
+
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(maxLimit))
+	if err != nil {
+		return 0, err
+	}
+	randomNumberInt := int(randomNumber.Int64())
+
+	// Handling integers between 0, 10^(n-1) .. for n=4, handling cases between (0, 999)
+	if randomNumberInt <= lowLimit {
+		randomNumberInt += lowLimit
+	}
+
+	// Never likely to occur, kust for safe side.
+	if randomNumberInt > int(maxLimit) {
+		randomNumberInt = int(maxLimit)
+	}
+	return randomNumberInt, nil
+}
